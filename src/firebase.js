9684f6709ae4bad4dca11e7083c20f04bbdadefb
Walisson Rodrigues <contact@walisson.com>
2020-11-30 14:55:46 +1100

    Firebase configuration file creation

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA0RK-hDdZFBdDRdw1b5wEbdGrdoSSIDTw",
+    authDomain: "react-crud-b58c0.firebaseapp.com",
+    databaseURL: "https://react-crud-b58c0.firebaseio.com",
+    projectId: "react-crud-b58c0",
+    storageBucket: "react-crud-b58c0.appspot.com",
+    messagingSenderId: "851808995738",
+    appId: "1:851808995738:web:791a34eba03d8f36db1223"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
