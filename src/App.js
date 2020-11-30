7d4f9c159157a18c2f4a7afad02c4aea4bddada5
Walisson Rodrigues <contact@walisson.com>
2020-11-30 13:53:12 +1100

    First commit - Initial files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>React JS and Firebase CRUD</h1>
+    </div>
+  );
+}
+
+export default App;
